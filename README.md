42c735ce1b74c6cb607e30fa71bb67514a1007a0
Grant T-F <Grant T-F>
2016-09-09 10:17:19 +0100

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wpfnotifyicon
 repository for a WPF windowless app with a notify icon
+
+running this solution will show just an read icon in the system tray, which will show *hello world* if you hover over the icon
---
+++ WpfNotifyIcon/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfNotifyIcon
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon tb;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            tb = (TaskbarIcon)FindResource("MyNotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            tb.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
+    }
+}
